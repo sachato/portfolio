42a36a3218af9a01c3654c54a4728bc710775b50
SachaTORTELLI <SachaTORTELLI@otami.fr>
2023-06-06 11:46:51 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccueilComponent},
+  { path: 'accueil', component: AccueilComponent},
+  { path: 'projet', component: ProjetsComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ContactComponent } from './contact/contact.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProjetsComponent,
+    ContactComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
